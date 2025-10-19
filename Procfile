8cdde1a22be553339da6f962fde8e429949e78f8
vikrantlall <vikrant@variableconstant.com>
2025-10-18 20:42:22 -0400

    update

---
+++ Procfile
@@ -0,0 +1,5 @@
+web: gunicorn app:app --bind 0.0.0.0:$PORT
+```
+
+---
+
--- app.py
+++ app.py
@@ -766,4 +766,7 @@ def export_csv():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=6969)
+    import os
+
+    port = int(os.environ.get('PORT', 6969))
+    app.run(host='0.0.0.0', port=port, debug=False)
